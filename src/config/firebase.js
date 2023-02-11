b2dee90d31d9608e4ff61c5b31f76c2c73ac7a99
TruongNguyen <truongmoba157@gmail.com>
2023-02-11 21:50:49 +0700

    devploy

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,6 +2,8 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
+// dotenv.config()
+
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
